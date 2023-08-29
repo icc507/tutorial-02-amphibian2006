d338e9ddae0dfa8ad61dd33aafbb127638ef72a3
Ignacio Morales <70661952+amphibian2006@users.noreply.github.com>
2023-08-29 09:41:53 -0400

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -7,5 +7,16 @@
 #         20 90 hola jiji 77
 #La salida debe ser
 #         (77, 'jiji', 'hola', 90, 20)
-t = input()
-print(t)
+t = input().split()
+s = []
+# invertimos el orden
+for i in range(len(t)):
+    s.append(t[len(t)-i-1])
+# hacemos los numeros enteros
+for i in range(len(s)):
+    if s[i].isdigit():
+        s[i] = int(s[i])
+# volvemos a s tupla
+r=tuple(s)
+
+print(r)
